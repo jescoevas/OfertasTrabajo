3d06b0fd3f36dccd5b55c897fea0243090483e9f
Jesus Coello <jescoevas@hotmail.com>
2020-09-23 16:08:05 +0200

    Front con navbar

---
+++ front/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    $("#wrapper").toggleClass("toggled");
+    $("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+    });
+  }
+
+}
